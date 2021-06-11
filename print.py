4cbcbc3268483ca7d60daaff8e135a405511f946
Samrat132 <84699138+Samrat132@users.noreply.github.com>
2021-06-11 11:59:01 +0545

    Initial commit

---
+++ print.py
@@ -0,0 +1,7 @@
+name =input("enter your name:")
+age = int(input("enter age:"))
+
+print("hello my name is " + name + " and i am " +str(age)+ "years old")
+print("hello my name is", name, " and i am ", age, "years old")
+print("hello my name is %s and i am %d years old" %(name,age))
+print("hell0  my name is {} and i am {} years old".formate(name,age))
