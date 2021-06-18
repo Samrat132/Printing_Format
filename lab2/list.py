064669d84464415ee9964c9f2b819d6dcaa075be
Samrat132 <84699138+Samrat132@users.noreply.github.com>
2021-06-18 14:57:26 +0545

    list

---
+++ lab2/list.py
@@ -0,0 +1,12 @@
+#Check whether 5 is in list of first 5 natural numbers or not.
+# Hint: List => [1,2,3,4,5]
+
+List=[1,2,3,4,5]
+num = int(input('Enter the number:'))
+
+if num !=5:
+ print(f"Number isn't in the list")
+
+else:
+ print(f"Number is in the list")
+
