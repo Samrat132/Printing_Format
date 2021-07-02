0aa0e4f89bcd3c50698c2ea26f6805b42d785059
Samrat132 <84699138+Samrat132@users.noreply.github.com>
2021-07-02 16:08:28 +0545

    ONE

---
+++ Lab4/three.py
@@ -0,0 +1,17 @@
+#Write a Python program to guess a number between 1 to 9.
+# Note :User is prompted to enter a guess.
+# If the user guesses wrong then the prompt appears again until the guess is correct, on successful guess,
+# user will get a "Well guessed!" message, and the program will exit.
+
+import random
+
+num=int(input("enter a number : "))
+
+ran= random.randint(1, 10)
+
+while num!= ran:
+
+    ran= int(input('Guess a number between 1 to 10 : '))
+
+print('Well guessed!')
+
---
+++ Lab4/two.py
@@ -0,0 +1,13 @@
+#Write a Python program to convert temperatures to and from celsius, fahrenheit.C = (5/9) * (F -32)
+
+temp=input('Enter the temperature which you like to convert : ')
+cel='C'
+fah='F'
+
+while temp.upper()=="C":
+ print(F"The temperature in Celsius is{(temp%9)/5+32} ")
+
+
+print(F"The temperature in Fahrenheit is{(temp-32)*5/9}")
+
+
