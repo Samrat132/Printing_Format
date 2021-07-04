724c2f7da1f4592fbf3c43217f56f2f503310af7
Samrat132 <84699138+Samrat132@users.noreply.github.com>
2021-07-04 16:02:59 +0545

    one

--- List/one.py
+++ List/one.py
@@ -1,8 +1,4 @@
 # Write a Python program to sum all the items in a list.
 
 lst=[1,2,3,4,5]
-product=1
-
-for i in lst:
-    product=product*i
-    print(product,end=" ")
+print(sum(lst))
--- List/two.py
+++ List/two.py
@@ -1,5 +1,8 @@
 # Write a Python program to multiplies all the items in a list.
 
 lst=[1,2,3,4,5]
-result=lst.prod(lst)
-print(result)
+product=1
+
+for i in lst:
+    product=product*i
+    print(product,end=" ")
