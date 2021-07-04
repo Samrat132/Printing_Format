883d44a4323fbd916f749b7282324c685c777d93
Samrat132 <84699138+Samrat132@users.noreply.github.com>
2021-07-04 15:58:48 +0545

    one

---
+++ List/one.py
@@ -0,0 +1,8 @@
+# Write a Python program to sum all the items in a list.
+
+lst=[1,2,3,4,5]
+product=1
+
+for i in lst:
+    product=product*i
+    print(product,end=" ")
--- lab2/list.py
+++ lab2/list.py
@@ -1,5 +1,5 @@
 #Check whether 5 is in list of first 5 natural numbers or not.
-# Hint: List => [1,2,3,4,5]
+# Hint: Lab1 => [1,2,3,4,5]
 
 List=[1,2,3,4,5]
 num = int(input('Enter the number:'))
