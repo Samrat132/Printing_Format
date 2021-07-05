5b39efb70e492ca02a782ca3d0745fecc889ebd8
Samrat132 <84699138+Samrat132@users.noreply.github.com>
2021-07-05 09:34:47 +0545

    eight

---
+++ List/eight.py
@@ -0,0 +1,7 @@
+#  Write a Python program to print a specified list after removing the 0th, 4th and 5th elements
+
+list=[1,2,3,4,5,6,7,8,9,10]
+list.remove(list[0])
+list.remove(list[4])
+list.remove(list[5])
+print(list)
